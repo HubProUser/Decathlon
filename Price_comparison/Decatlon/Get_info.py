ac04743b6ccfe29fce516d5f60531fa5c2cb7054
HubProUser <allhubpro@gmail.com>
2023-02-21 18:28:58 +0200

    last_commit

--- Price_comparison/Decatlon/Get_info.py
+++ Price_comparison/Decatlon/Get_info.py
@@ -3,18 +3,18 @@ import urllib.request
 import re
 
 
-def request(link):
+def request(link):     # get decathlon site code
     r = urllib.request.urlopen(link)
     soup = bs4.BeautifulSoup(r, 'html.parser')
     return soup
 
 
-def category(soup):
+def category(soup):    # all sports with useless info
     categories = soup.find_all('li', {'class': 'root root-3 category-sports'})
     return categories
 
 
-def sports(categories):
+def sports(categories):   # all the sport links and names
     for sport in categories:
         sport = sport.find_all('a', href=re.compile('https://www.decathlon.bg/'))
         return sport
@@ -22,6 +22,7 @@ def sports(categories):
 
 data = request(link='https://www.decathlon.bg/')
 cat = category(data)
+#print(cat)              # try to use both prints to see what each does
 sport = sports(cat)
 print(sport)
 
