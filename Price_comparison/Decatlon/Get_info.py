c43eaa0f3f87b4160586f2e3ec5cf410d92cc595
HubProUser <allhubpro@gmail.com>
2023-02-21 12:55:29 +0200

    all_categories

--- Price_comparison/Decatlon/Get_info.py
+++ Price_comparison/Decatlon/Get_info.py
@@ -1,21 +1,18 @@
 import bs4
 import urllib.request
+import gspread
 
 
-def get_links(a):
-    decathlon = 'https://www.decathlon.bg'
-    r = urllib.request.urlopen(decathlon)
+def decathlon():
+    link = 'https://www.decathlon.bg/'
+    r = urllib.request.urlopen(link)
     soup = bs4.BeautifulSoup(r, 'html.parser')
-    link = soup.find('li', {'class': f'root root-{a}'}).find('a')['href']
-    return link
+    return soup
 
 
-def links():
-    men_stocks = get_links(a='4')
-    women_stocks = get_links(a='5')
-    children_stocks = get_links(a='6')
-    accessories = get_links(a='7')
-    return men_stocks, women_stocks, children_stocks, accessories
+def category(soup):
+    categories = soup.find_all('ul', {'class': 'submenu submenu-depth-2'})
+    return categories
 
 
 
@@ -23,3 +20,13 @@ def links():
 
 
 
+
+
+
+
+
+
+data = decathlon()
+links = category(data)
+sport = sports(links)
+print(sport)
